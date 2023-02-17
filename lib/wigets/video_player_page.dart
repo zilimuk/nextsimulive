ca97bc5a56099ddfb9a9ec9cfcff975fc4f3ac9d
PAISHA DEVELOPER <zilimuk@gmail.com>
2023-02-17 18:11:41 +0300

    video player

--- lib/wigets/video_player_page.dart
+++ lib/wigets/video_player_page.dart
@@ -1,109 +1,89 @@
-import 'package:better_player/better_player.dart';
 import 'package:flutter/material.dart';
 import 'package:simulive/movies/model/video_streaming.dart';
+import 'package:video_player/video_player.dart';
 
 class VideoPlayerScreen extends StatefulWidget {
   final VideoStreaming videoUrl;
-  const VideoPlayerScreen({super.key, required this.videoUrl});
+
+  const VideoPlayerScreen({Key? key, required this.videoUrl}) : super(key: key);
+
   @override
-  // ignore: library_private_types_in_public_api
   _VideoPlayerScreen createState() => _VideoPlayerScreen();
 }
 
 class _VideoPlayerScreen extends State<VideoPlayerScreen> {
-  late final BetterPlayerController _betterPlayerController;
-  final GlobalKey _betterPlayerKey = GlobalKey();
+  late VideoPlayerController _controller;
 
   @override
   void initState() {
     super.initState();
-    BetterPlayerDataSource betterPlayerDataSource = BetterPlayerDataSource(
-      BetterPlayerDataSourceType.network,
-      widget.videoUrl.data![0].media!.first,
-      cacheConfiguration: const BetterPlayerCacheConfiguration(
-        useCache: true,
-        preCacheSize: 10 * 1024 * 1024,
-        maxCacheSize: 10 * 1024 * 1024,
-        maxCacheFileSize: 10 * 1024 * 1024,
-
-        ///Android only option to use cached video between app sessions
-        // key: "testCacheKey",
-      ),
-      // bufferingConfiguration: const BetterPlayerBufferingConfiguration(
-      //   minBufferMs: 50000,
-      //   maxBufferMs: 13107200,
-      //   bufferForPlaybackMs: 2500,
-      //   bufferForPlaybackAfterRebufferMs: 5000,
-      // ),
-    );
-
-    _betterPlayerController = BetterPlayerController(
-      const BetterPlayerConfiguration(
-        fit: BoxFit.contain,
-        handleLifecycle: true,
-        autoPlay: true,
-        fullScreenByDefault: false,
-        looping: true,
-        showPlaceholderUntilPlay: true,
-        autoDetectFullscreenAspectRatio: false,
-        autoDispose: false,
-        // deviceOrientationsAfterFullScreen: [
-        //   DeviceOrientation.landscapeLeft,
-        //   DeviceOrientation.landscapeRight
-        // ],
-        controlsConfiguration: BetterPlayerControlsConfiguration(
-          progressBarHandleColor: Color.fromARGB(255, 255, 0, 0),
-          progressBarPlayedColor: Color.fromARGB(255, 255, 0, 0),
-          loadingColor: Color.fromARGB(255, 255, 0, 0),
-          showControlsOnInitialize: true,
-          enableOverflowMenu: true,
-          enablePip: true,
-          enableFullscreen: true,
-
-          // overflowMenuCustomItems: [
-          //   BetterPlayerOverflowMenuItem(
-          //     Icons.account_circle_rounded,
-          //     "Custom element",
-          //     () => print("Click!"),
-          //   )
-          // ],
-        ),
-      ),
-      betterPlayerDataSource: betterPlayerDataSource,
-    );
-    // _betterPlayerController!
-    //     .setupDataSource(betterPlayerDataSource)
-    //     .then((response) {
-    //   // Source loaded successfully
-    //   videoLoading = false;
-    // }).catchError((error) async {
-    //   // Source did not load, url might be invalid
-    //   inspect(error);
-    // });
-    _betterPlayerController.isPictureInPictureSupported();
-    _betterPlayerController.enablePictureInPicture(_betterPlayerKey);
-    _betterPlayerController.preCache(betterPlayerDataSource);
-    _betterPlayerController.setOverriddenAspectRatio(1.0);
-    super.initState();
+    _controller =
+        VideoPlayerController.network(widget.videoUrl.data![0].media!.first)
+          ..initialize().then((_) {
+            setState(() {});
+          });
   }
 
   @override
   void dispose() {
-    // ignore: todo
     super.dispose();
-    _betterPlayerController.dispose();
+    _controller.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return BetterPlayerMultipleGestureDetector(
-      onTap: (() {}),
-      child: AspectRatio(
-        aspectRatio: 16 / 9,
-        child: BetterPlayer(
-          controller: _betterPlayerController,
-          key: _betterPlayerKey,
-        ),
+    return Scaffold(
+      backgroundColor: Colors.black26,
+      body: Center(
+        child: _controller.value.isInitialized
+            ? Stack(
+                children: [
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      SizedBox(
+                        child: AspectRatio(
+                          aspectRatio: _controller.value.aspectRatio,
+                          child: Center(
+                            child: FittedBox(
+                              fit: BoxFit.contain,
+                              child: SizedBox(
+                                width: _controller.value.size.width,
+                                height: _controller.value.size.height,
+                                child: VideoPlayer(_controller),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            IconButton(
+                              icon: Icon(
+                                _controller.value.isPlaying
+                                    ? Icons.pause
+                                    : Icons.play_arrow,
+                                color: Colors.white,
+                                size: 50,
+                              ),
+                              onPressed: () {
+                                setState(() {
+                                  _controller.value.isPlaying
+                                      ? _controller.pause()
+                                      : _controller.play();
+                                });
+                              },
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              )
+            : Container(),
       ),
     );
   }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,12 +9,10 @@ import connectivity_plus
 import path_provider_foundation
 import shared_preferences_macos
 import sqflite
-import wakelock_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
-  WakelockMacosPlugin.register(with: registry.registrar(forPlugin: "WakelockMacosPlugin"))
 }
